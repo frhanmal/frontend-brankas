dfe59b9d6de1029ea6dc2d973c80b5f416f565c3
frhanmal <farhanakmal268@gmail.com>
2024-02-29 18:05:01 +0700

    first commit

--- src/components/brankas.js
+++ src/components/brankas.js
@@ -89,6 +89,76 @@ export function DataTabel() {
   );
 }
 
+export function DataAll() {
+  const [sensorData, setSensorData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          // "https://grafik-sensor.vercel.app/api/sensors/getDataSensor"
+          "https://server-brankas.vercel.app/api/encrypted/getDataEncrypted"
+        );
+        setSensorData(response.data);
+      } catch (error) {
+        console.error("Error fetching sensor data:", error);
+      }
+    };
+
+    fetchData();
+
+    const interval = setInterval(fetchData, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Row className="justify-content-md-center">
+      <div class="product-catagories-wrapper pt-3">
+        <Container>
+          <div class="product-catagory-wrap">
+            <Container>
+              <Card className="mb-3 catagory-card">
+                <table className="table">
+                  <thead>
+
+                  {/* Gambar ketika pintu terbuka/ tertutup */}
+                    <tr>
+                      <th scope="col">No</th>
+                      <th scope="col">Status</th>
+                      {/* <th scope="col">Teks</th>
+                      <th scope="col">Enkripsi</th> */}
+                      {/* <th scope="col">Deksripsi</th> */}
+                      {/* <th scope="col">Riwayat</th> */}
+                      <th scope="col">Tanggal</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {sensorData.map((data, index) => (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        {/* <td>{data.status_pintu}</td>
+                        <td>{data.teks_asli}</td> */}
+                        <td>{data.encryptedText}</td>
+                        {/* <td>{data.teks_dekripsi}</td> */}
+                        {/* <td>{data.riwayat_pin}</td> */}
+                        <td>
+                          {data.createdAt !== null
+                            ? new Date(data.createdAt).toLocaleString()
+                            : "N/A"}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </Card>
+            </Container>
+          </div>
+        </Container>
+      </div>
+    </Row>
+  );
+}
 
 export function RiwayatPin() {
   const [sensorData, setSensorData] = useState([]);
